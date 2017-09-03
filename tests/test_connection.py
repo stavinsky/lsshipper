d5742f5330c4d62ae6631cbbae7f43aba2e248f5
stavinsky <stavinsky@gmail.com>
2017-09-03 13:54:17 +0300

    make test_connection to check restore connect after disconnect

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -4,17 +4,26 @@ from common.state import State
 from connection import logstash_connection
 from common.config import config
 
+counter = 0
+
 
 @pytest.mark.asyncio
 async def test_read_common_file(event_loop, unused_tcp_port):
     test = list()
     state = State()
+    global counter
+    counter = 0
 
     async def handler(reader, writer):
+        global counter
+        counter += 1
         while not state.need_shutdown:
             line = await reader.readline()
             if not line or line == b'':
                 break
+            if (counter % 2) == 0:
+                writer.close()
+                return
             test.append(line)
         done.set()
         writer.close()
@@ -25,9 +34,13 @@ async def test_read_common_file(event_loop, unused_tcp_port):
     config['connection']['port'] = port
     config['ssl']['enable'] = False
     queue = asyncio.Queue(loop=event_loop)
-    await queue.put("Message 1\n")
-    await queue.put("Message 2\n")
-    await queue.put("Message 3\n")
+    test_messages = list()
+
+    for i in range(100):
+        m = "Message {}\n".format(i)
+        test_messages.append(m)
+        await queue.put(m)
+
     server = await asyncio.start_server(
         handler, host='127.0.0.1', port=port, loop=event_loop)
     client = asyncio.ensure_future(logstash_connection(
@@ -36,6 +49,6 @@ async def test_read_common_file(event_loop, unused_tcp_port):
     state.shutdown()
     await client
     await done.wait()
-    assert len(test) == 3
+    assert len(test) == len(test_messages)
     server.close()
     await server.wait_closed()
