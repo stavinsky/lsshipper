8253a953f4203961e6105dce303b947f23a5a652
stavinsky <stavinsky@gmail.com>
2017-09-04 17:46:39 +0300

    prepare for pip

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ def parse_requirements(requirements):
 
 setup(
     name="lsshipper",
-    version="0.0.1",
+    version="0.1",
     author="Anton Stavinsky",
     author_email="stavinsky at gmail dot com",
     description=("Very basic python logstash shipper"),
     license="MIT",
     keywords="logstash filebeat",
-    url="https://bitbucket.org/stavinsky/logstash_shipper",
+    url="https://github.com/stavinsky/lsshipper/archive/0.1.tar.gz",
     packages=find_packages(),
     tests_require=['pytest', 'pytest-asyncio', ],
     setup_requires=['pytest-runner', ],
@@ -31,4 +31,9 @@ setup(
         "License :: OSI Approved :: MIT License",
     ],
     install_requires=parse_requirements("requirements.txt"),
+    entry_points={
+        'console_scripts': [
+            'lsshipper = lsshipper.start:main'
+            ]
+        },
 )
